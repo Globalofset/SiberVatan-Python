c9901051e1aa6e3c88e27b4dd01e4410dce15ec6
Globalofset <155827435+Globalofset@users.noreply.github.com>
2024-01-10 19:09:22 +0300

    first commit
    first commit

---
+++ deneme1.py
@@ -0,0 +1,35 @@
+#print("Hello World")
+'''
+sayi=10
+sayi=100
+print(sayi)
+yas=10
+YAS=100
+print(YAS)
+
+sayi = -7
+print("mutlak deger",abs(sayi))
+
+
+
+us = pow(2,3)
+print("girdiginiz sayınn ussu",us)
+
+
+from math import sqrt
+
+sayi = 25
+karekok = sqrt (sayi)
+print("girdiğiniz sayının karekoku",karekok)
+
+'''
+from math import sqrt,ceil,floor
+ondalik=10.25
+asagi=ceil(ondalik)
+yukari=floor(ondalik)
+print("yukarı",yukari)
+print("asagi",asagi)
+
+
+
+
